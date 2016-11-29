2f136fecb18e238c7296d13d27f38622d46eb5a3
Lekan Omotayo <lekan.omotayo@interswitchgroup.com>
2016-11-29 20:00:06 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# paycode_php
+Interswitch Paycode PHP Library
+
+
+## Installation
+
+Install the latest version with
+
+```bash
+$ composer require Paycode
+```
+
+## Basic Usage
+
+```php
+<?php
+
+use \Paycode;
+
+// Initialize Interswitch object
+$CLIENT_ID = "IKIA9614B82064D632E9B6418DF358A6A4AEA84D7218";
+$CLIENT_SECRET = "XCTiBtLy1G9chAnyg0z3BcaFK4cVpwDg/GTw2EmjTZ8=";
+$paycode = new Paycode($CLIENT_ID, $CLIENT_SECRET);
+$accessToken = ''; // Get user access token from Interswitch Passport (https://sandbox.interswitchng.com/passport/oauth/authorize) or https://saturn.interswitchng.com/passport/oauth/authorize.
+
+// Build request data
+$cardExpDate = "1909"; // Select Payment Token Expiry Date
+$cvv = "123"; // Select Payment Token CVV
+$pin = "1234"; // Select Payment Token PIN
+$pwmChannel = "ATM"; // Paycode is for use at ATM. Possible options: ATM, POS, AGENT
+$tokenLifeInMin = "90"; // Paycode expiry time
+$otp = "1234"; // Paycode One Time PIN
+$amt = "500000"; // Paycode Amount
+$tranType = "Withdrawal"; // Paycode for Cash Withdrawal. Possible options: "Withdrawal", "Payment"
+$fep = 'WEMA'; // Assigned by Interswitch
+
+// Get eWallet
+$eWalletResp = $paycode->getEWallet($accessToken);
+$httpResp = $eWalletResp['HTTP_CODE'];
+$respBody = $eWalletResp['RESPONSE_BODY'];
+$json_resp = json_decode($respBody);
+  
+if($httpResp == 200)
+{
+  // Select one of the many eWallet returned
+  $paymentToken = $json_resp->paymentMethods[1]->token;
+  
+  // Generate a Paycode from the payment token selected
+  $paycodeResp = $paycode->generateWithEWallet($accessToken, $paymentToken, $cardExpDate, $cvv, $pin, $amt, $fep, $tranType, $pwmChannel, $tokenLifeInMin, $otp);
+  $paycodeHttpResp = $paycodeResp['HTTP_CODE'];
+  $paycodeRespBody = $paycodeResp['RESPONSE_BODY'];
+  $json_paycode_resp = json_decode($paycodeRespBody);
+  if($paycodeHttpResp == 200 || $paycodeHttpResp == 201)
+  {
+    $paycodeToken = $json_paycode_resp->payWithMobileToken; // Paycode
+    $tokenLifeMin = $json_paycode_resp->tokenLifeTimeInMinutes; // Expiry Time
+  }
+}
+```
+
+
+## Third Party Packages
+
+- interswitch
+- JWT
+
+## About
+
+### Requirements
+
+- Intersiwtch SDK works with PHP 5.0 or above.
+
+### Author
+
+Lekan Omotayo - <developer@interswitchgroup.com><br />
+
+### License
+
+Paycode SDK is licensed under the ISC License
+
+
+
---
+++ src/Paycode.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Description of Paycode
+ *
+ * @author Lekan.Omotayo
+ */
+
+require_once 'vendor/autoload.php';
+
+if(!class_exists("JWT"))
+{
+  require_once __DIR__.'/lib/JWT.php';
+}
+
+use Interswitch\Interswitch as Interswitch;
+use \JWT as JWT;
+
+class Paycode {
+
+private $clientId;
+private $clientSecret;
+private $environment;
+private $interswitch;
+
+const HTTP_CODE = "HTTP_CODE";
+const RESPONSE_BODY = "RESPONSE_BODY";
+
+
+public function __construct($clientId, $clientSecret, $environment = null) {
+  $this->clientId = $clientId;
+  $this->clientSecret = $clientSecret;
+  if ($environment !== null) {
+    $this->environment = $environment;
+  }
+  $this->interswitch = new Interswitch($this->clientId, $this->clientSecret, $this->environment);
+}
+
+
+function getEWallet($accessToken)
+{
+  return $this->interswitch->sendWithAccessToken("api/v1/ewallet/instruments", "GET", $accessToken);
+}
+
+function generateWithEWallet($accessToken, $paymentMethodIdentifier, $expDate, $cvv, $pin, $amt, $fep, $tranType, $pwmChannel, $tokenLifeInMin, $otp)
+{
+
+  //echo "<br>Pin: " . $pin;
+  //echo "<br>CVV: " . $cvv;
+  //echo "<br>Exp Date: " . $expDate;
+
+  $ttid = Paycode::Randomize();
+  $decoded = JWT::decode($accessToken, null, false);
+  $msisdn = $decoded->mobileNo;
+  $secure = $this->interswitch->getSecureData(null, $expDate, $cvv, $pin, null, $msisdn, $ttid);
+
+  $secureData = $secure['secureData'];
+  $pinData = $secure['pinBlock'];
+  $macData = $secure['mac'];
+
+  //echo "<br>Secure Data: " . $secure['secureData'];
+  //echo "<br>Pin Block: " . $secure['pinBlock'];
+  //echo "<br>Mac: " . $secure['mac'];
+
+  $httpHeader = [
+     'frontendpartnerid: '. $fep
+  ];
+
+  //echo '<br>Mobile No: ' . $msisdn;
+
+  $req = array(
+    "amount" => $amt,
+    "ttid" => $ttid,
+    "transactionType" => $tranType,
+    "paymentMethodIdentifier" => $paymentMethodIdentifier,
+    "payWithMobileChannel" => $pwmChannel,
+    "tokenLifeTimeInMinutes" => $tokenLifeInMin,
+    "oneTimePin" => $otp,
+    "pinData" => $pinData,
+    "secure" => $secureData,
+    "macData" => $macData
+   );
+   $jsonReq = json_encode($req);
+
+  //echo "<br>Generate Paycode Req: " . $jsonReq;
+  //echo "<br>Headers 1: ";
+  //print_r($httpHeader);
+  return $this->interswitch->sendWithAccessToken("api/v1/pwm/subscribers/" . $msisdn . "/tokens", "POST", $accessToken, $jsonReq, $httpHeader);
+}
+
+static function Randomize() {
+    return mt_rand(0, 999);    
+}
+
+}
+
